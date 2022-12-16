b272c3058ae899bbcb758ec34955b5281d6469d4
LouiseM <louise.moller@rabobank.nl>
2022-12-16 11:10:43 +0100

    Small assignment fixes

--- Test/Testing_Exercises/CalculatorTest.java
+++ Test/Testing_Exercises/CalculatorTest.java
@@ -2,6 +2,7 @@ package Testing_Exercises;
 
 import Main.Java.Testing_Exercises.Calculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,18 +13,22 @@ class CalculatorTest {
     void shouldMultiply() {
         Calculator calculator = new Calculator();
 
-        assertEquals(2.0, calculator.multiply(1, 2), "error");
-        assertEquals(162.0, calculator.multiply(27, 6), "error");
-        assertEquals(404.2, calculator.multiply(9.4, 43), "error");
+        assertAll(
+                ()-> assertEquals(2.0, calculator.multiply(1, 2), "error"),
+                ()-> assertEquals(162.0, calculator.multiply(27, 6), "error"),
+                ()-> assertEquals(404.2, calculator.multiply(9.4, 43), "error")
+        );
     }
 
     @Test
     void shouldDivide() {
         Calculator calculator = new Calculator();
 
-        assertEquals(0.47777777777777775, calculator.divide(4.3, 9), "error");
-        assertEquals(56.4, calculator.divide(84.6, 1.5), "error");
-        assertEquals(4.5, calculator.divide(9, 2), "error");
+        assertAll(
+                ()-> assertEquals(0.47777777777777775, calculator.divide(4.3, 9), "error"),
+                ()-> assertEquals(56.4, calculator.divide(84.6, 1.5), "error"),
+                ()-> assertEquals(4.5, calculator.divide(9, 2), "error")
+        );
     }
 
     @Test
@@ -36,12 +41,15 @@ class CalculatorTest {
     }
 
     @Test
+    @Disabled("The shouldSum() test has been disabled")
     void shouldSum() {
         Calculator calculator = new Calculator();
 
-        assertEquals(3.0, calculator.sum(1, 2), "error");
-        assertEquals(14.0, calculator.sum(9, 5), "error");
-        assertEquals(9.0, calculator.sum(6, 3), "error");
+        assertAll(
+                ()-> assertEquals(3.0, calculator.sum(1, 2), "error"),
+                ()-> assertEquals(14.0, calculator.sum(9, 5), "error"),
+                ()-> assertEquals(9.0, calculator.sum(6, 3), "error")
+        );
     }
 
     @Test
@@ -49,7 +57,13 @@ class CalculatorTest {
         Calculator calculator = new Calculator();
 
         assertEquals(-3.0, calculator.subtract(6, 9), "error");
-        assertEquals(-3.0, calculator.subtract(6, 9), "error");
+        assertEquals(7.0, calculator.subtract(9, 2), "error");
         assertEquals(8.0, calculator.subtract(45, 37), "error");
+
+        assertAll(
+                ()-> assertEquals(-3.0, calculator.subtract(6, 9), "error"),
+                ()-> assertEquals(7.0, calculator.subtract(9, 2), "error"),
+                ()-> assertEquals(8.0, calculator.subtract(45, 37), "error")
+        );
     }
 }
